d90f03d477da2f6af7ea11f6b0fab5762e1d65d6
Brandon Mellus <mellusbrandon@gmail.com>
2019-06-10 20:49:36 -0400

    Setup communication with client and server in development mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,14 @@
 // https://www.typescriptlang.org/docs/handbook/react-&-webpack.html
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+function setDevTool() {
+  const ENV = process.env.NODE_ENV;
+  if (ENV === 'testing') return 'inline-source-map';
+  if (ENV === 'production') return 'source-map';
+  return 'eval-source-map';
+}
 
 module.exports = {
     entry: "./src/index.tsx",
@@ -6,7 +16,7 @@ module.exports = {
         filename: "bundle.js",
         path: __dirname + "/dist"
     },
-    devtool: "source-map",
+    devtool: setDevTool(),
 
     resolve: {
         extensions: [".ts", ".tsx", ".js", ".json"]
@@ -15,8 +25,37 @@ module.exports = {
     module: {
         rules: [
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+            { test: /\.html$/, use: [{ loader: 'html-loader', options: { minimize: true } }] },
+            {
+              test: /\.scss$/,
+              use: [
+                { loader: MiniCssExtractPlugin.loader },
+                { loader: 'css-loader' },
+                { loader: 'sass-loader' }
+              ]
+            }
         ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html'
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFileName: '[id].css'
+      })
+    ],
+    devServer: {
+      contentBase: path.join(__dirname),
+      historyApiFallback: true,
+      port: 5000,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true
+        }
+      }
     },
     externals: {
         "react": "React",
