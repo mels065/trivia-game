c00c00c363d32d2187eac5ab559df36243adfaaa
Brandon Mellus <mellusbrandon@gmail.com>
2019-06-27 13:16:35 -0400

    Finish Player class

--- src/sockets/classes/Player/index.ts
+++ src/sockets/classes/Player/index.ts
@@ -3,16 +3,15 @@ import { Answer } from "../../../enums";
 export default class PlayerClass {
     public displayName: string;
     public ready: boolean;
+    public currentAnswer: Answer | null;
 
     private score: number;
-    private currentAnswer: Answer | null;
 
     constructor(displayName: string) {
         this.displayName = displayName;
         this.ready = false;
 
         this.score = 0;
-        this.currentAnswer = null;
     }
 
     public getScore(): number {
--- src/sockets/classes/Player/test.ts
+++ src/sockets/classes/Player/test.ts
@@ -1,6 +1,8 @@
 import { expect } from "chai";
 import { mock, when } from "ts-mockito";
 
+import { Answer } from "../../../enums";
+
 import PlayerClass from "./index";
 
 describe("PlayerClass", () => {
@@ -12,10 +14,14 @@ describe("PlayerClass", () => {
 
     it("has a public `displayName`", () => {
         expect(player.displayName).to.equal("John Doe");
+        player.displayName = "Jane Doe";
+        expect(player.displayName).to.equal("Jane Doe");
     });
 
     it("has a public `ready` boolean property", () => {
         expect(player.ready).to.equal(false);
+        player.ready = true;
+        expect(player.ready).to.equal(true);
     });
 
     it("has methods for getting and setting `score` private property", () => {
@@ -29,7 +35,10 @@ describe("PlayerClass", () => {
         expect(player.getScore()).to.equal(3);
     });
 
-    it.skip("sets `currentAnswer` with `selectAnswer` method", () => {
-        // CODE
+    it.skip("has `currentAnswer` property that can be either null or the enum Answer", () => {
+        expect(player.currentAnswer).to.equal(null);
+
+        player.currentAnswer = Answer.B;
+        expect(player.currentAnswer).to.equal(Answer.B);
     });
 });
