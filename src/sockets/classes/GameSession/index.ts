b402e2c1869d5be533b03fc119ae6976171e1e7f
Brandon Mellus <mellusbrandon@gmail.com>
2019-06-29 18:42:54 -0400

    Allow players to join or leave session; destroy session if no players

--- src/sockets/classes/GameSession/index.ts
+++ src/sockets/classes/GameSession/index.ts
@@ -1,13 +1,38 @@
 import shortid = require("shortid");
+import Player from "../Player";
 
 export default class GameSession {
     public static sessions: {
         [key: string]: GameSession,
     } = {};
 
+    public readonly id: string;
+    public readonly players: {
+        [key: number]: Player,
+    } = {};
+
+    private totalPlayersJoined: number;
+
     constructor() {
-        GameSession.sessions[shortid()] = this;
+        this.id = shortid();
+        GameSession.sessions[this.id] = this;
+
+        this.totalPlayersJoined = 0;
     }
 
-    public destroy(): void {}
+    public addPlayer(player: Player): void {
+        // Remember, appended `++` increments after assignment
+        this.players[this.totalPlayersJoined++] = player;
+    }
+
+    public removePlayer(id: number) {
+        delete this.players[id];
+        if (Object.keys(this.players).length === 0) {
+            this.destroy();
+        }
+    }
+
+    public destroy(): void {
+        delete GameSession.sessions[this.id];
+    }
 }
--- src/sockets/classes/GameSession/test.ts
+++ src/sockets/classes/GameSession/test.ts
@@ -1,34 +1,50 @@
 import { expect } from "chai";
-import { rewiremock } from "../../../../rewiremock";
+import * as shortid from "shortid";
+import { mock, reset } from "ts-mockito";
 
-const shortIdStub = () => "test-id";
-rewiremock("shortid").with(shortIdStub);
-rewiremock(() => require("shortid")).with(shortIdStub);
+import Player from "../Player";
 import GameSession from "./index";
 
 describe("GameSession class", () => {
     let gameSession: GameSession;
+    let mockedPlayer: Player;
+
     beforeEach(() => {
+        mockedPlayer = mock(Player);
+
         GameSession.sessions = {};
         gameSession = new GameSession();
     });
 
+    afterEach(() => {
+        reset(mockedPlayer);
+    })
+
     it("has a static object of all game sessions", () => {
-        // expect(Object.keys(GameSession.sessions).length).to.equal(1);
-        expect(GameSession.sessions).to.deep.equal({
-            "test-id": gameSession,
-        });
+        expect(Object.keys(GameSession.sessions).length).to.equal(1);
     });
 
     it("can destroy itself and remove itself from static object", () => {
         gameSession.destroy();
-        expect(Object.keys(GameSession.sessions).length).to.equal(1);
+        expect(Object.keys(GameSession.sessions).length).to.equal(0);
     });
 
-    it.skip("has an object of players that can join or leave", () => {
-        // expect(Object.keys(gameSession.players)).to.equal(0);
+    it("has an object of players that can join or leave, and deletes session if no players", () => {
+        expect(Object.keys(gameSession.players).length).to.equal(0);
 
-        // gameSession.addPlayer();
+        gameSession.addPlayer(new Player("John Doe"));
+        expect(Object.keys(gameSession.players).length).to.equal(1);
+
+        gameSession.addPlayer(new Player("Jane Doe"));
+        expect(Object.keys(gameSession.players).length).to.equal(2);
+
+        gameSession.removePlayer(0);
+        expect(Object.keys(gameSession.players).length).to.equal(1);
+        expect(Object.keys(GameSession.sessions).length).to.equal(1);
+
+        gameSession.removePlayer(1);
+        expect(Object.keys(gameSession.players).length).to.equal(0);
+        expect(Object.keys(GameSession.sessions).length).to.equal(0);
     });
 
     it.skip("has a question array that makes an API call to OpenTriviaDB to retrieve them", () => {
